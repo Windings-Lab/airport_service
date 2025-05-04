5e8939744bec79f1d3fad59eb2230b5b45183a14
Gaster <janton20001@gmail.com>
2025-05-04 15:58:52 +0300

    feat: integrate django-debug-toolbar for dev use
    - Add django-debug-toolbar to dev dependencies
    - Configure URLs to include debug toolbar in DEBUG mode
    - Update dev settings to include debug toolbar app and middleware
    - Define INTERNAL_IPS for local development

--- app/settings/dev.py
+++ app/settings/dev.py
@@ -5,3 +5,8 @@ ALLOWED_HOSTS: list[str] = []
 SECRET_KEY = "django-insecure-wi+16heuxz+hf72!s_h19o=#n=0mf3hzse*o98e6qoby7y41fr"
 
 DEBUG = True
+
+INSTALLED_APPS.append("debug_toolbar")
+MIDDLEWARE.insert(1, "debug_toolbar.middleware.DebugToolbarMiddleware")
+
+INTERNAL_IPS = ["127.0.0.1"]
--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import include, path
+from django.conf import settings
 
 urlpatterns = [
     path("account/", include("account.urls", namespace="account")),
     path("airport/", include("airport.urls", namespace="airport")),
 ]
+
+if settings.DEBUG:
+    from debug_toolbar.toolbar import debug_toolbar_urls
+    urlpatterns += debug_toolbar_urls()
