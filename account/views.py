4c416588a4fc44b274511af5319b68dd9ce518e2
Gaster <janton20001@gmail.com>
2025-05-04 18:15:29 +0300

    refactor: remove custom token serializer and view
    - Remove CustomTokenObtainPairSerializer and related imports
    - Update token endpoint to use default TokenObtainPairView
    - Simplify account serializers and views by removing unused code

--- account/views.py
+++ account/views.py
@@ -1,14 +1,9 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
-from .serializers import UserSerializer, CustomTokenObtainPairSerializer
-
-
-class CustomTokenObtainPairView(TokenObtainPairView):
-    serializer_class = CustomTokenObtainPairSerializer
+from .serializers import UserSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
